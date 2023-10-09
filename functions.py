5d12517ec88e5133be829bd40c99944bfd99232e
Andrea Mancini <mancioman2@gmail.com>
2023-10-09 15:21:41 +0200

    - correct randomness by adjusting system message - tune random sleep and max thread number - seem working

--- functions.py
+++ functions.py
@@ -1,33 +1,17 @@
 import concurrent.futures
-import copy
-import time
 import re
+import time
+import random
+
 import openai
 import srt
 import os
-import tiktoken
 
-# Limit for the maximum tokens per API call
-MAX_TOKENS_PER_CALL = 3500
+# ai_model = 'gpt-4'
+ai_model = 'gpt-3.5-turbo'
+# ai_model = 'gpt-3.5-turbo-0301'
 
-# Translate each subtitle
-translated_subtitles = []
-
-# to split subs
-splitter = '\n\n'
-
-ai_model = 'gpt-4'
-
-encoding = tiktoken.encoding_for_model(ai_model)
-
-current_tokens = 0
-# user_message = ""  # Initialize an empty user message
-translated_text = ""
-italian_text = ""
-
-
-# def count_tokens(text):
-#     return len(encoding.encode(text))
+subtitles = []
 
 
 def make_dirs(input_folder, output_folder):
@@ -53,41 +37,16 @@ def parse_subs(input_file):
         return list(srt.parse(file.read()))
 
 
-# def append_subs(text):
-#     global user_message
-#     user_message += splitter + text
-
-
-# def split_text_to_array():
-#     # Split the input text using the delimiter
-#     text_list = translated_text.split(splitter)
-#
-#     return [text.strip() for text in text_list]
-
-
-# def text_is_too_long():
-#     global current_tokens
-#     current_tokens += count_tokens(user_message)
-#     return current_tokens > MAX_TOKENS_PER_CALL
-
-
-# def reset_count():
-#     global user_message
-#     global current_tokens
-#     user_message = ""
-#     current_tokens = 0
-
-
 def prepare_path(input_file, output_folder, target_lang):
     # Construct the output file path
     base_name = os.path.splitext(os.path.basename(input_file))[0]
     return os.path.join(output_folder, f"{base_name}_{target_lang.upper()}.srt")
 
 
-def save_output(output_file, subtitles):
+def save_output(output_file, subs):
     # Write the translated subtitles to the output .srt file
-    with open(output_file, 'a', encoding='utf-8') as file:
-        file.write(srt.compose(subtitles))
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(srt.compose(subs))
 
 
 def run_model(message):
@@ -95,59 +54,45 @@ def run_model(message):
         model=ai_model,
         messages=message,
         temperature=0,
-        max_tokens=256,
+        max_tokens=50,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
 
 
-def translate_subtitle(user_message, input_language, target_lang):
-    system_role = [{'role': 'system', 'content': f'Translate the following subtitle from "{input_language}" to '
-                                                 f'"{target_lang}":'}]
+def translate_subtitle(user_message, input_language, target_language):
+    system_role = [{'role': 'system', 'content': f'Please translate every subtitle I will send you '
+                                                 f'from {input_language} to {target_language}. '
+                                                 f'Do not be creative. Just repeat the same text in case '
+                                                 f'you cannot translate'}]
     user_role = [{'role': 'user', 'content': user_message}]
 
-    # global translated_text
-    #
-    # if not translated_text:
     message = system_role + user_role
-    # else:
-    #     message = user_role
 
-    # if translated_text:
-    # time.sleep(1)
+    time.sleep(random.uniform(0.1, 0.3))
 
-    # completion = run_model(message)
-    #
-    # return completion.choices[0].message.content.strip()
+    completion = run_model(message)
 
-    return "sticazzi"
+    return completion.choices[0].message.content.strip()
 
-    # global italian_text
-    # italian_text += user_message
+    # return str(index)
 
 
 def translate_concurrent(index, subtitle, input_language, target_lang):
-    global translated_subtitles
-    translated_subtitles[index].content = translate_subtitle(clean_string(subtitle.content), input_language, target_lang)
+    translated_subtitle = translate_subtitle(clean_string(subtitle.content), input_language, target_lang)
+    print("sub number: " + str(index) + " translated")
+    return index, translated_subtitle
 
 
 # Define a function to translate a single subtitle file using GPT-3.5 Turbo with 4K context
 def translate(input_file, output_folder, input_language, target_lang, api_key):
-    # Initialize the OpenAI API client with the "gpt-3.5-turbo" engine
     openai.api_key = api_key
 
+    global subtitles
     subtitles = parse_subs(input_file)
 
-    global translated_subtitles
-    translated_subtitles = copy.deepcopy(subtitles)
-
-    # for subtitle in subtitles:
-    #     subtitle.content = translate_subtitle(subtitle.content, input_language, target_lang)
-    #     print("translated Sub number: " + str(subtitle.index) + " on total of " + str(len(subtitles)))
-
-    # Specify the maximum number of threads (adjust as needed)
-    max_threads = 1
+    max_threads = 10
 
     # Create a ThreadPoolExecutor with the specified maximum number of threads
     with concurrent.futures.ThreadPoolExecutor(max_threads) as executor:
@@ -156,23 +101,11 @@ def translate(input_file, output_folder, input_language, target_lang, api_key):
                              for index, subtitle
                              in enumerate(subtitles)]
 
-        # Wait for all tasks to complete
-        concurrent.futures.wait(translation_tasks)
-
-        # append_subs(clean_string(subtitle.content))
-
-    #     if text_is_too_long():
-    #         translate_block(input_language, target_lang)
-    #         reset_count()
-    #
-    # if user_message:
-    #     translate_block(input_language, target_lang)
-
-    # text_list = split_text_to_array()
-    #
-    # for idx, subtitle in enumerate(subtitles):
-    #     subtitle.content = text_list[idx]
+        # Wait for all tasks to complete and collect the results
+        for future in concurrent.futures.as_completed(translation_tasks):
+            index, translated_subtitle = future.result()
+            subtitles[index].content = translated_subtitle
 
     output_file = prepare_path(input_file, output_folder, target_lang)
 
-    save_output(output_file, translated_subtitles)
+    save_output(output_file, subtitles)
